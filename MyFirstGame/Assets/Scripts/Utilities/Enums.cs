1f0f83e7aff3cc8c6473e6fc803406a6dea69157
MitchellMorse <mitchellmorse008@gmail.com>
2016-04-02 20:20:30 -0600

    Slope effect

---
+++ MyFirstGame/Assets/Scripts/Utilities/Enums.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+[Flags]
+public enum SpriteState
+{
+    Normal = 0,
+    Shrinking = 1,
+    Growing = 2,
+    DownSlope = 4,
+    UpSlope = 8,
+    RightSlope = 16,
+    LeftSlope = 32
+}
