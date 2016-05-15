2b28addbb8d2ca6f1cdcafa34128b4a2dc077c7b
MitchellMorse <mitchellmorse008@gmail.com>
2016-05-15 13:30:23 -0600

    Adding damaging wall modifiers

---
+++ MyFirstGame/Assets/Scripts/Walls/DamagingWall.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using Assets.Scripts.PlayerClasses;
+using Assets.Scripts.Utilities;
+
+public class DamagingWall : MonoBehaviour
+{
+    protected void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag(Tags.Player.ToString()))
+        {
+            PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+            player.StopVelocity();
+
+            Vector2 dir = transform.position.CalculateVectorTowards(player.transform.position);
+
+            player.moveHorizontal = dir.x;
+            player.moveVertical = dir.y;
+            player.AddForce(500);
+            player.DamageObject();
+        }
+    }
+}
